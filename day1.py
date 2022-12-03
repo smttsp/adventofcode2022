9eb75f4e768f0d139a245839f509ceaea06bbc75
smttsp <samettaspinarx@gmail.com>
2022-12-03 10:02:05 -0500

    day1 challange

---
+++ day1.py
@@ -0,0 +1,23 @@
+
+def get_all_deers():
+    file1 = open('./data/day1.txt', 'r')
+    lines = file1.readlines()
+
+    max_deer = 0
+    cur_deer = 0
+    all_deers = []
+    for line in lines:
+
+        if line.strip():
+            cur_deer += int(line.strip())
+        else:
+            all_deers.append(cur_deer)
+            max_deer = max(max_deer, cur_deer)
+            cur_deer = 0
+    return all_deers
+
+all_deers = get_all_deers()
+top_deers = sorted(all_deers, reverse=True)
+
+print(top_deers[0])
+print(sum(top_deers[:3]))
