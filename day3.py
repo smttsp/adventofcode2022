4a498d6c202b9e62fa55517419100b7043c7d14e
smttsp <samettaspinarx@gmail.com>
2022-12-03 10:13:43 -0500

    [CLN] day3 cleanup

--- day3.py
+++ day3.py
@@ -8,6 +8,15 @@ lower_ord = ord("a") - 1
 upper_ord = ord("A") - 1
 
 
+def get_val(letter):
+    cur_val = 0
+    if letter.islower():
+        cur_val += (ord(letter) - lower_ord)
+    else:
+        cur_val += (ord(letter) - upper_ord) + 26
+    return cur_val
+
+
 def get_part1(lines):
     val = 0
     for line in lines:
@@ -16,25 +25,18 @@ def get_part1(lines):
         second_cmp = line[half_len:]
 
         intersections = set(first_cmp).intersection(set(second_cmp))
-        for inter in intersections:
-            if inter.islower():
-                val += (ord(inter) - lower_ord)
-            else:
-                val += (ord(inter) - upper_ord) + 26
+        for letter in intersections:
+            val += get_val(letter)
     return val
 
 
 def get_part2(lines):
     val = 0
     for line1, line2, line3 in zip(lines[::3], lines[1::3], lines[2::3]):
-
         intersections = set(line1).intersection(set(line2)).intersection(set(line3))
         print(intersections)
-        for inter in intersections:
-            if inter.islower():
-                val += (ord(inter) - lower_ord)
-            else:
-                val += (ord(inter) - upper_ord) + 26
+        for letter in intersections:
+            val += get_val(letter)
     return val
 
 print(get_part1(lines))
